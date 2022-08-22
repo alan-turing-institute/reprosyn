c3c661473b907abb3678d94f2405e04975637ed0
callummole <cmole@turing.ac.uk>
2022-08-22 11:40:42 +0100

    delete run.py

--- src/reprosyn/run.py
+++
@@ -1,9 +0,0 @@
-def run(gen, dataset=None, size=None, output_dir="./", **kwargs):
-    generator = gen(
-        dataset=dataset, output_dir=output_dir, size=size, **kwargs
-    )
-    generator.preprocess()
-    generator.generate()
-    generator.postprocess()
-    generator.save()
-    return generator
