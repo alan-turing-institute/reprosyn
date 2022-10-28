cfd8f0881fdb09932bc41ec63ba5bb8c9400d943
callummole <cmole@turing.ac.uk>
2022-10-28 11:28:14 +0100

    add some metadata validation and subsetting

--- src/reprosyn/dataset.py
+++ src/reprosyn/dataset.py
@@ -35,12 +35,61 @@ class Dataset:
 
         self.data = self.read_dataset(dataset)
         self.metadata = self.read_metadata(metadata)
+        self.validate_metadata(self.metadata)
+        self._check_correspondence()
         self.data = self.data.astype(self.dtypes_from_metadata(self.metadata))
+
+    def _check_correspondence(self):
+        """Checks if data columns are represented in metadata
+
+        If there are more metadata columns, metadata is subsetted.
+
+        Raises
+        ------
+        Exception
+            If data columns are not represented
+        """
+
+        metadata_cols = [col["name"] for col in self.metadata]
+        print(f"mcols: {metadata_cols}")
+        columns = list(self.data.columns)
+        print(f"data columns: {columns}")
+
+        if set(columns) <= set(metadata_cols):
+            # subset metadata.
+            self.metadata = [c for c in self.metadata if c["name"] in columns]
+        elif set(columns) > set(metadata_cols):
+            raise Exception(
+                f"The following columns are not represented in metedata: {set(columns)-set(metadata_cols)}"
+            )
+
+    @staticmethod
+    def validate_metadata(metadata):
+        """Ensures metadata corresponds to `Data Format <https://privacy-sdg-toolbox.readthedocs.io/en/latest/dataset-schema.html>`_
+
+        Parameters
+        ----------
+        metadata : list[dict]
+            Should be a list of dictionaries.
+        """
+
+        assert isinstance(metadata, list)
+        assert all(isinstance(c, dict) for c in metadata)
+        assert all(
+            [
+                all(
+                    k in col.keys() for k in ["name", "type", "representation"]
+                )
+                for col in metadata
+            ]
+        )
+        assert all(isinstance(x["name"], str) for x in metadata)
+        # TODO: further assertions could check for 'type' and 'representation' entries
 
     @staticmethod
     def dtypes_from_metadata(metadata):
         """
-        our metadata uses the prive datatypes, see: https://privacy-sdg-toolbox.readthedocs.io/en/latest/dataset-schema.html
+        our metadata uses the TAPAS `Data Format <https://privacy-sdg-toolbox.readthedocs.io/en/latest/dataset-schema.html>`_
         """
 
         representation_map = {
@@ -79,7 +128,7 @@ class Dataset:
             return _json_from_url(metadata)
         else:
             raise Exception(
-                "Metadata needs to be a real file, a url to a file, or a dictionary"
+                "Metadata needs to be a real file, a url to a file, or a list[dict]"
             )
 
 
